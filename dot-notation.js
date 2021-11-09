f0e82294d93440c903e3b19f2ffe920ba0ca1b6e
Jerika Kanz <jerikacapila@gmail.com>
2021-11-09 10:31:42 +0100

    Use Dot Notation to Access the Properties of an Object

---
+++ dot-notation.js
@@ -0,0 +1,16 @@
+/* Use dot notation to access the properties of an object */
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+};
+console.log(duck.name);
+
+// Challenge
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+console.log(dog.name); // output : Spot
+console.log(dog.numLegs); // output : 4
