b62e6d3a88ea837cbc2eeb885a14569249b356aa
Jerika Kanz <jerikacapila@gmail.com>
2021-11-09 10:27:52 +0100

    Create a Basic JavaScript Object

---
+++ create_object.js
@@ -0,0 +1,14 @@
+/* Properties are qualities that define what makes up an object. 
+  Note that similar objects share the same properties, but may have different values for those properties.*/
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+};
+
+// Challenge
+
+let dog = {
+  name: "Papi",
+  numLegs: 4,
+};
