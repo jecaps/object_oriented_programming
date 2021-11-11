08504ffb645f19f958160395f69db17a966fa149
Jerika Kanz <jerikacapila@gmail.com>
2021-11-11 11:00:06 +0100

    Understand the Immediately Invoked Function Expression (IIFE)

---
+++ iife.js
@@ -0,0 +1,14 @@
+(function () {
+  console.log("Chirp, chirp!");
+})();
+
+/* This is an anonymus function expression that executes right awaym and outputs "Chirp, chirp!" immediately.
+  Note that the function has no name and is not stored in variable.
+  The two parentheses at the end of the function expression cause it to be immediately executed or invoked.
+  This pattern is called immediately invoked function expression or IIFE. */
+
+// Challenge
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
