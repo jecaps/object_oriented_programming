5f1ada2ec423068b9e3193080ece3eafc62a4fe6
Jerika Kanz <jerikacapila@gmail.com>
2021-11-11 11:15:12 +0100

    Use an IIFE to Create a Module

---
+++ iife-create-module.js
@@ -0,0 +1,49 @@
+/* Immediately invoked function expression is often used to group related functionality into a single object or module. */
+
+/*
+function glideMixin(obj) {
+  obj.glide = function() {
+    console.log("Gliding on the water");
+  };
+}
+function flyMixin(obj) {
+  obj.fly = function() {
+    console.log("Flying, wooosh!");
+  };
+}
+*/
+
+let motionModule = (function () {
+  return {
+    glideMixin: function (obj) {
+      obj.glide = function () {
+        console.log("Gliding on the water");
+      };
+    },
+    flyMixin: function (obj) {
+      obj.fly = function () {
+        console.log("Flying, wooosh!");
+      };
+    },
+  };
+})();
+
+motionModule.glideMixin(duck);
+duck.glide();
+
+// Challenge
+
+let funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      obj.isCute = function () {
+        return true;
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log("Singning to an awesome tune");
+      };
+    },
+  };
+})();
